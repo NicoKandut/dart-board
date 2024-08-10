1bc7d4469902249c8da5f3d8e4705d8b71af34f9
Manuel Tippler <mtippler@student.tugraz.at>
2024-08-10 19:32:07 +0200

    take photos and stores it

---
+++ poc/image_gethering/usb_webcam.py
@@ -0,0 +1,12 @@
+import cv2
+from datetime import datetime
+
+PATH = "data/board-{}.png".format(datetime.now().strftime("%d%m%y%H%M%S"))
+
+cam = cv2.VideoCapture(4) #can mby change on other devices
+
+result, image = cam.read() 
+
+if result: 
+    cv2.imwrite(PATH, image) 
+ 
