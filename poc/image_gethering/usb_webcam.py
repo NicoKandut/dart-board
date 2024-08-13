aef4c14586ef2f0c2f06e00407833de8528a6b8f
Manuel Tippler <mtippler@student.tugraz.at>
2024-08-13 15:56:45 +0200

    now callable as function

--- poc/image_gethering/usb_webcam.py
+++ poc/image_gethering/usb_webcam.py
@@ -1,12 +1,12 @@
 import cv2
 from datetime import datetime
 
-PATH = "data/webcam/board-{}.png".format(datetime.now().strftime("%d%m%y%H%M%S"))
+def take_photos():
+    PATH = "data/webcam/board-{}.png".format(datetime.now().strftime("%d%m%y%H%M%S"))
 
-cam = cv2.VideoCapture(4) #can mby change on other devices
+    cam = cv2.VideoCapture(4) #can mby change on other devices
 
-result, image = cam.read() 
+    result, image = cam.read() 
 
-if result: 
-    cv2.imwrite(PATH, image) 
- 
+    if result: 
+        cv2.imwrite(PATH, image)
