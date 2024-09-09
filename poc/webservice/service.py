b33a5b8d3e46342063a26ae84a2e750a93449103
Tippler Manuel <mtippler@student.tugraz.at>
2024-09-09 15:55:15 +0200

    removed the communication with shared memory

--- poc/webservice/service.py
+++ poc/webservice/service.py
@@ -1,30 +1,8 @@
-from flask import Flask, render_template
-from shared_memory_dict import SharedMemoryDict
-import atexit
 import os
+from flask import Flask, render_template
 
-shm_data: SharedMemoryDict = SharedMemoryDict(name="data", size=128)
 app = Flask(__name__, template_folder=os.getcwd() + "/poc/webservice/templates", static_folder=os.getcwd() + "/poc/webservice/static")
-
-def cleanup():
-    shm_data.shm.close()
-    shm_data.shm.unlink()
-    del shm_data
-
-data = {
-    "score": 0,
-    "remainding": 0,
-    "player": 0,
-    "game_state": 0,
-    "last_dart_x_pos": 0,
-    "last_dart_y_pos": 0 
-    }
-
-for key in data:
-    shm_data[key] = 0
-
-atexit.register(cleanup)
 
 @app.route("/")
 def hello_world():
-    return render_template('_base.html', data=shm_data)
+    return render_template('_base.html')
